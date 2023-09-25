29b3bb703e28eccd90d2b4b6c69bdcaea95505f1
Nathan <kiprotichnathan@yahoo.com>
2023-09-25 16:14:52 +0300

    task done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = 'Bob';
+
+
+function upperCaseName(){
+
+}
+function setBestCustomer(){
+    bestCustomer = 'poor Bob';
+
+}
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe Bob';
+
+    
+    
+    const leastCustomer = 'Nay';
+    {
+
+    }
+}
